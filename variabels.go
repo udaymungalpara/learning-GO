9f6be779dfe1a27758b7704638210a225d37cb01
udaymungalpara <udayhn2005@gmail.com>
2024-01-26 22:05:54 +0530

    palying with variables

---
+++ variabels.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var name string = "Uday"
+	fmt.Println(name)
+	fmt.Printf("The type of variable is:%T \n", name)
+
+	var isgood bool = false
+	fmt.Println(isgood)
+	fmt.Printf("The type of variable is:%T \n", isgood)
+
+	var num int = 500
+	fmt.Println(num)
+	fmt.Printf("The type of variable is:%T \n", num)
+
+	var floa float64 = 5000.66462
+	fmt.Println(floa)
+	fmt.Printf("The type of variable is:%T \n", floa)
+}
