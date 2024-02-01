392b56d74bb0f1e04d05a80e361e76acd03d414c
udaymungalpara <udayhn2005@gmail.com>
2024-02-01 20:37:32 +0530

    working with slices

---
+++ 07Slices/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main()  {
+	
+	fmt.Println("Slices on the GOlang: ")
+
+	var bois = []string{"Uday","Raj","Rahul"}
+	fmt.Printf("The type is :%T\n",bois)
+	
+	bois = append(bois, "sujal","Nikunj")
+	
+	fmt.Println(bois)
+
+	bois = append(bois[1:])
+	fmt.Println(bois)
+
+	Numbers := make([]int, 5)
+
+	
+	Numbers[0] = 767
+	Numbers[1] = 567
+	Numbers[2] = 987
+	Numbers[3] = 231
+	Numbers[4] = 546
+
+	fmt.Println(Numbers)
+
+	sort.Ints(Numbers)
+	fmt.Println(Numbers)
+}
